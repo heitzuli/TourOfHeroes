5a4a18579bfa85e40709b13e3f018c98424184e3
heidi <heidi.poels@gmail.com>
2023-08-14 13:58:20 +0300

    Add mock heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./Hero";
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Ant-Man' },
+  { id: 13, name: 'Aquaman' },
+  { id: 14, name: 'Batman' },
+  { id: 15, name: 'Asterix' },
+  { id: 16, name: 'Batwoman' },
+  { id: 17, name: 'The Hulk' },
+  { id: 18, name: 'Dorian Gray' },
+  { id: 19, name: 'Invisible woman' },
+  { id: 20, name: 'Wonderwoman' },
+];
