fb7a41b51fc5c66b5f0d5c29bcffb97600857bac
heidi <heidi.poels@gmail.com>
2023-08-16 17:40:54 +0300

    Make getHEros observable

--- angular-tour-of-heroes/src/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from "../app/Hero";
 import { HEROES } from "../app/mock-heroes";
+import { HeroService } from "../app/hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +10,18 @@ import { HEROES } from "../app/mock-heroes";
 })
 export class HeroesComponent {
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+  constructor(private heroService: HeroService) {}
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
