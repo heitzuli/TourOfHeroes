d5b6d5e10baaf77f1638a0cde7ccb9e479d172b0
heidi <heidi.poels@gmail.com>
2023-08-24 13:39:54 +0300

    Add routing

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from 'src/heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterOutlet,
+    RouterLink
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
